0bdab79564f68e69821bab3395b0119d4254e523
Andrzej Rafalski <anraf1001@gmail.com>
2021-07-28 10:30:24 +0200

    Test case - mem write during oled init

--- Core/UT/OLED_SSD1306-ut.cpp
+++ Core/UT/OLED_SSD1306-ut.cpp
@@ -1,7 +1,14 @@
 #include <gtest/gtest.h>
 
+#include <memory>
+
+#include "I2C_MockHandler.hpp"
 #include "OLED_SSD1306.hpp"
 
-TEST(testSuite, shouldRunTest) {
-    ASSERT_TRUE(true);
+using namespace ::testing;
+
+TEST(oledTestSuite, shouldRunMemWriteDuringInit) {
+    auto hi2c = std::make_shared<NiceMock<I2C_MockHandler>>();
+    EXPECT_CALL(*hi2c, Mem_Write(0x3C, 0x00, 1, NotNull(), 1)).Times(AtLeast(1));
+    OLED_SSD1306 oled{hi2c};
 }
